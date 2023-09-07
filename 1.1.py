051ff6f6eafe81230b985d5fd632158cdc759f2d
Sudharavi44 <144125782+Sudharavi44@users.noreply.github.com>
2023-09-07 07:18:37 +0500

    Add files via upload

--- 1.1.py
+++ 1.1.py
@@ -1,6 +1,6 @@
 # impliment a recursive function to calculate the factorial of given number
 #factorial of 5 = 1 * 2 * 3 * 4 * 5
-#farmula = n x (n-1)
+#formula = n x (n-1)
 def fact_rec(n):
  if n==0 or n==1:
   return 1
